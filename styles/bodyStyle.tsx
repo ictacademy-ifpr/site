eae0b966c184d2976e195034c7a43b6b35b56a02
Athos Ishikawa <athos.iskw@gmail.com>
2023-11-07 14:48:23 -0300

    images resized

--- styles/bodyStyle.tsx
+++ styles/bodyStyle.tsx
@@ -44,7 +44,7 @@ export const Banner = styled.section`
             width: 100%;
             height: 100%;
             img{
-                object-position: bottom;
+                object-position: center;
             }
         }
     }
